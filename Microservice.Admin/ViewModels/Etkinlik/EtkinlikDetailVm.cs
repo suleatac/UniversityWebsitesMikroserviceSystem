275e815d1bd4b9f267c7e0f6382ac9f7b65c7007
suleatac2022@gmail.com <suleatac2022@gmail.com>
2026-09-22 16:53:51 +0300

    zdf

--- Microservice.Admin/ViewModels/Etkinlik/EtkinlikDetailVm.cs
+++ Microservice.Admin/ViewModels/Etkinlik/EtkinlikDetailVm.cs
@@ -34,7 +34,6 @@ namespace Microservice.Admin.ViewModels.Etkinlik
         public string? Link { get; set; }
 
         [StringLength(500, ErrorMessage = "Resim URL en fazla 500 karakter olabilir.")]
-        [Url(ErrorMessage = "Geçerli bir resim URL giriniz.")]
         public string? ResimUrl { get; set; }
 
         public int GosterimSayisi { get; set; } = 0;
