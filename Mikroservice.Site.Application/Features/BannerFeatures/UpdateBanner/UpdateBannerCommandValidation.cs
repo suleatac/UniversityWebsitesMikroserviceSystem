1136e5ab8ec822dc681dd8581db21748d7a898ea
suleatac2022@gmail.com <suleatac2022@gmail.com>
2026-09-22 14:37:33 +0300

    banner için validation düzeltmesi yapıldı.

--- Mikroservice.Site.Application/Features/BannerFeatures/UpdateBanner/UpdateBannerCommandValidation.cs
+++ Mikroservice.Site.Application/Features/BannerFeatures/UpdateBanner/UpdateBannerCommandValidation.cs
@@ -23,8 +23,7 @@ namespace Mikroservice.Site.Application.Features.BannerFeatures.UpdateBanner
                 .NotEmpty().WithMessage("Kısa açıklama boş olamaz.")
                 .MaximumLength(500).WithMessage("Kısa açıklama en fazla 500 karakter olabilir.");
 
-            RuleFor(x => x.IcerikMetni)
-                .NotEmpty().WithMessage("İçerik metni boş olamaz.");
+
 
             RuleFor(x => x.ResimUrl)
                 .MaximumLength(500)
